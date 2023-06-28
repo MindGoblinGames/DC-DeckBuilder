164d385a937da30bae92a83bbbfd2dba79355e62
Xenotater <kgamer5000@gmail.com>
2023-06-27 23:00:32 -0500

    updated some assets

--- dc-deckbuilder/src/app/home/home.component.ts
+++ dc-deckbuilder/src/app/home/home.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 export class HomeComponent {
   // cards = ["Strike", "Block", "Assist", "Dash", "Dying Breath"];
   // status = ["Intact", "Intact", "Intact", "Exhausted", "Destroyed"];
+
+  navRules() {
+    let rulesUrl = "../assets/Rules/Dungeon_Crawlers_v0.5.0.pdf"; //update w/ new versions
+    window.open(rulesUrl, "_blank");
+  }
 }
