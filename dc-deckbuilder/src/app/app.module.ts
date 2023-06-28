680ef5defcf587958aa67de1f0492e2e3458d1b6
Xenotater <kgamer5000@gmail.com>
2023-06-28 15:37:48 -0500

    added some dependencies

--- dc-deckbuilder/src/app/app.module.ts
+++ dc-deckbuilder/src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DragDropModule, CdkDrag } from '@angular/cdk/drag-drop'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CardComponent } from './card/card.component';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons'
 
 @NgModule({
   declarations: [
@@ -14,9 +19,16 @@ import { CardComponent } from './card/card.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DragDropModule,
+    CdkDrag,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor (library: FaIconLibrary) {
+    library.addIconPacks(fas, far);
+  }
+}
--- dc-deckbuilder/src/app/home/home.component.ts
+++ dc-deckbuilder/src/app/home/home.component.ts
@@ -1,16 +1,44 @@
 import { Component } from '@angular/core';
+import { IconPrefix } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  // cards = ["Strike", "Block", "Assist", "Dash", "Dying Breath"];
-  // status = ["Intact", "Intact", "Intact", "Exhausted", "Destroyed"];
+  loggedIn = false;
+  mgLogoSrc = '../assets/mind_goblin_hidden.webp';
+  iconStyle: IconPrefix = 'far';
+  
+  cards = ['Strike', 'Block', 'Assist', 'Dash', 'Dying Breath'];
+  status = ['Intact', 'Intact', 'Intact', 'Exhausted', 'Destroyed'];
 
   navRules() {
-    let rulesUrl = "../assets/Rules/Dungeon_Crawlers_v0.5.0.pdf"; //update w/ new versions
-    window.open(rulesUrl, "_blank");
+    let rulesUrl = '../assets/Rules/Dungeon_Crawlers_v0.5.0.pdf'; //update w/ new versions
+    window.open(rulesUrl, '_blank');
+  }
+
+  navMG() {
+    console.log('send to MG hub');
+  }
+
+  async login() {
+    console.log('login');
+    this.loggedIn = true;
+    return;
+  }
+  
+  async navProfile() {
+    if (!this.loggedIn)
+      await this.login();
+    console.log('route to profile');
+  }
+
+  toggleMG(mode: string) {
+    if (mode == 'on')
+      this.mgLogoSrc = '../assets/mind_goblin_icon.webp';
+    else if (mode == 'off')
+      this.mgLogoSrc = '../assets/mind_goblin_hidden.webp';
   }
 }
